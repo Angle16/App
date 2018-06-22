29ea942120dd597f50034a37cf9a849bacbca2c4
Your Name <you@example.com>
2018-06-22 11:07:48 +0800

    '22'

---
+++ src/DataServer/MessageManager.js
@@ -0,0 +1,15 @@
+import {
+postMessageURL,
+postCommentURL,
+allMessagesURL,
+allCommentsURL
+}from './URLconfig';
+class MessageManager{
+    async postMessage(title,content,images){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
