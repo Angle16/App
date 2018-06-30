39cda5f650b9b231aa3384ad57038ae4b81b2cb3
Your Name <you@example.com>
2018-06-30 13:16:54 +0800

    '22'

---
+++ src/Screen/LoginScreen.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react'
+import {
+Button,
+Toast,
+NavBar,
+InputItem,
+WingBlank,
+WhiteSpace,
+List
+}from 'antd-mobile'
+import userManager from '../DataServer/UserManager';
+export default class LoginScreen extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            username:'',
+            password:''
+        }
+    }
+    render(){
+        return (
+            <div>
+                <NavBar
+                    mode="dark"
+                    icon={<Icon type="left" />}
+                    onLeftClick={() => { this.props.history.goBack() }}
+                >登录</NavBar>
+                <WhiteSpace />
+                <List>
+                    <InputItem
+                        type={'text'}
+                        value={this.state.username}
+                        onChange={(username) => { this.setState({ username }) }}
+                        placeholder={"请输入登录用户名"}
+                    >用户名
+                    </InputItem>
+                    <InputItem
+                        type={'text'}
+                        value={this.state.password}
+                        onChange={(password) => { this.setState({ password }) }}
+                        placeholder={'请输入登录密码'}
+                    >
+                        密码
+            </InputItem>
+                </List>
+            <WhiteSpace/>
+            <WingBlank>
+                <Button
+                type={'primary'}
+                onClick={async()=>{
+                    const result=await userManager.register(this.state.username,this.state.password);
+                    console.log(result);
+                    if(result.success===false){
+                        Toast.fail(result.errorMessage);
+                        return;
+                    }
+                    this.props.history.replace('/HomeScreen');
+                }}
+                >
+                    </Button>
+                </WingBlank>
+            </div>
+        )
+    }
+}
